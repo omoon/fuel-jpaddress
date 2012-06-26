f2d813f95c172bc0a304de39c10bb360bf2639e1
omoon <omooon@gmail.com>
2012-06-26 11:08:14 +0900

    bootstrap 忘れてた。

--- bootstrap.php
+++ bootstrap.php
@@ -2,5 +2,6 @@
 Autoloader::add_core_namespace('Jpaddress');
 
 Autoloader::add_classes(array(
-	'Jpaddress\\Ken' => __DIR__.'/classes/ken.php',
+	'Jpaddress\\Ken'   => __DIR__.'/classes/ken.php',
+	'Jpaddress\\Jusho' => __DIR__.'/classes/Jusho.php',
 ));
