eb79ac982295d433d5e40d47501ac22528060784
Sotaro OMURA <omooon@gmail.com>
2012-06-27 20:15:25 +0900

    削除漏れ。

--- bootstrap.php
+++ bootstrap.php
@@ -3,6 +3,4 @@ Autoloader::add_core_namespace('Jpaddress');
 
 Autoloader::add_classes(array(
 	'Jpaddress\\Jpaddress'   => __DIR__.'/classes/jpaddress.php',
-	'Jpaddress\\Ken'   => __DIR__.'/classes/ken.php',
-	'Jpaddress\\Jusho' => __DIR__.'/classes/Jusho.php',
 ));
